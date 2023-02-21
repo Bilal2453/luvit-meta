d5aee65bf5698eb4a127931a14ecbb5ad8d803ef
Bilal2453 <belal2453@gmail.com>
2023-02-21 18:05:32 +0300

    resource: style consistency

--- library/resource.lua
+++ library/resource.lua
@@ -1,22 +1,32 @@
 ---@meta
 
+---
 ---Utilities for loading relative resources
+---
 local resource = {}
 
+---
 ---The full path of the file that called this function.
+---
 ---@type string
 resource.path = nil
 
+---
 ---The full path of the parent directory of the file that called this function.
+---
 ---@type string
 resource.dir = nil
 
+---
 ---Returns the absolute path given relative `path`.
+---
 ---@param path string
 ---@return string absPath
 function resource.resolve(path) end
 
+---
 ---Returns the content of the file at `path` as well as its absolute path.
+---
 ---@param path string
 ---@return string data, string absPath
 function resource.load(path) end
